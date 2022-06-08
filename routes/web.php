6f0205b2380f561cef06cb0c8def94cca136e407
Neno Arisma <nenoarisma@gmail.com>
2022-06-08 21:48:24 +0700

    menambahkan admin template untuk admin panel menggunakan mazer admin next update akan saya rombak untuk fitur-fitur yang kami perlukan saja

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/admin', function () {
-    return view('admin.index', [
+    return view('admin', [
         'title' => "Halaman Admin",
         'page' => "Halaman Admin"
     ]);

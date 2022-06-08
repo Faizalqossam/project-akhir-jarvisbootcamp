88befe36ed25fa9370bf1f94ea5830009f434799
Neno Arisma <nenoarisma@gmail.com>
2022-06-08 22:20:54 +0700

    Menambahkan RegisterController sekaligus dengan modelnya

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('register');
+    }
+
+    public function store(Request $request)
+    {
+        ddd($request);
+    }
+}

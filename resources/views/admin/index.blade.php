7c3dd41a24be36230c058d474b177c52b7d8a64b
Faizalqossam <alqassamf@gmail.com>
2022-06-26 11:27:42 +0700

    add action button in table

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,36 +1,42 @@
 @extends('admin.layout.main')
 
 @section('panel')
-    <div class="page-heading">
-        <h3>Peminat Lowongan</h3>
+<div class="page-heading">
+    <h3>Peminat Lowongan</h3>
+</div>
+<div class="card">
+    <div class="card-header">
+        <h4 class="card-title">Daftar Peminat Lowongan</h4>
     </div>
-    <div class="card">
-        <div class="card-header">
-            <h4 class="card-title">Daftar Peminat Lowongan</h4>
-        </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-lg">
-                    <thead>
-                        <tr>
-                            <th>Nama</th>
-                            <th>No Telp</th>
-                            <th>Alamat</th>
-                            <th>Lowongan Diminati</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($peminats as $peminat)
-                        <tr>
-                            <td>{{$peminat->nama}}</td>
-                            <td>{{$peminat->kontak}}</td>
-                            <td>{{$peminat->alamat}}</td>
-                            <td>{{$peminat->lowongans->judul_lowongan}}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-lg">
+                <thead>
+                    <tr>
+                        <th>Nama</th>
+                        <th>No Telp</th>
+                        <th>Alamat</th>
+                        <th>Lowongan Diminati</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($peminats as $peminat)
+                    <tr>
+                        <td>{{$peminat->nama}}</td>
+                        <td>{{$peminat->kontak}}</td>
+                        <td>{{$peminat->alamat}}</td>
+                        <td>{{$peminat->lowongans->judul_lowongan}}</td>
+                        <td>
+                            <a href=""><span class="badge bg-success">View</span></a>
+                            <a href=""><span class="badge bg-warning">Update</span>
+                            <a href=""><span class="badge bg-danger">Delete</span></a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
+</div>
 @endsection
